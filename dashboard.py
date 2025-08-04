24ad8bb4ad752d03e2d1f72dc8bfa6d46d0f2eff
Augustin GAUDIOT <augustin.gaudiot@sobotram.fr>
2025-08-04 08:27:24 +0200

    V1

--- dashboard.py
+++ dashboard.py
@@ -192,18 +192,28 @@ df_filtered = df.copy()
 
 with st.sidebar:
     st.header("🔍 Filtres")
+
     if 'Date_BE_dt' in df_filtered:
         min_date = df_filtered['Date_BE_dt'].min().date()  # date pure
         max_date = df_filtered['Date_BE_dt'].max().date()  # date pure
-        
+
+        st.subheader("Période Date_BE")  # Titre à la place du label
+
         date_range = datetime_range_picker(
-            label="Période Date_BE",
             key="date_range_picker",
             min_value=min_date,
             max_value=max_date,
             start_value=min_date,
             end_value=max_date
         )
+
+        if date_range is not None and len(date_range) == 2:
+            start_date, end_date = date_range  # ce sont des date
+            df_filtered = df_filtered[
+                (df_filtered['Date_BE_dt'].dt.date >= start_date) &
+                (df_filtered['Date_BE_dt'].dt.date <= end_date)
+            ]
+
 
         if date_range is not None and len(date_range) == 2:
             start_date, end_date = date_range  # ce sont des date
